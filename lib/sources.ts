5b4e9c21ca01cf7aea18a2ef7707e69a96c07219
Sergio <sergiolimacardoso@hotmail.com>
2026-07-18 09:43:11 -0300

    Add files via upload

--- lib/sources.ts
+++ lib/sources.ts
@@ -12,12 +12,12 @@ export const FEED_SOURCES: FeedSource[] = [
   { section: "Brasil", name: "G1", url: "https://g1.globo.com/rss/g1/brasil/" },
   { section: "Brasil", name: "Agência Brasil", url: "https://agenciabrasil.ebc.com.br/rss.xml" },
   { section: "Brasil", name: "CNN Brasil", url: "https://www.cnnbrasil.com.br/feed/" },
-  { section: "Mundo", name: "BBC News", url: "http://feeds.bbci.co.uk/news/world/rss.xml" },
-  { section: "Mundo", name: "Al Jazeera", url: "https://www.aljazeera.com/xml/rss/all.xml" },
-  { section: "Mundo", name: "NPR", url: "https://feeds.npr.org/1004/rss.xml" },
-  { section: "Economia", name: "BBC News", url: "http://feeds.bbci.co.uk/news/business/rss.xml" },
-  { section: "Tecnologia", name: "BBC News", url: "http://feeds.bbci.co.uk/news/technology/rss.xml" },
-  { section: "Ciência", name: "BBC News", url: "http://feeds.bbci.co.uk/news/science_and_environment/rss.xml" },
+  { section: "Mundo", name: "BBC News Brasil", url: "https://www.bbc.com/portuguese/index.xml" },
+  { section: "Mundo", name: "G1", url: "https://g1.globo.com/rss/g1/mundo/" },
+  { section: "Economia", name: "G1", url: "https://g1.globo.com/rss/g1/economia/" },
+  { section: "Economia", name: "CNN Brasil", url: "https://www.cnnbrasil.com.br/economia/feed/" },
+  { section: "Tecnologia", name: "G1", url: "https://g1.globo.com/rss/g1/tecnologia/" },
+  { section: "Ciência", name: "G1", url: "https://g1.globo.com/rss/g1/ciencia-e-saude/" },
 ];
 
 export const SECTION_ORDER: Section[] = ["Brasil", "Mundo", "Economia", "Tecnologia", "Ciência"];
